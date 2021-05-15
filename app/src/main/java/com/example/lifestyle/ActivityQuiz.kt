02f523bd1854384d04c4d269346c34e938ccb100
Tomasz Mnich <s16315@pjwstk.edu.pl>
2021-05-15 23:47:03 +0200

    quiz added

--- app/src/main/java/com/example/lifestyle/ActivityQuiz.kt
+++ app/src/main/java/com/example/lifestyle/ActivityQuiz.kt
@@ -26,12 +26,16 @@ class ActivityQuiz : AppCompatActivity() {
         quizQuestionTextView.text = quiz_1[0]
         quizAnswer1Button.textOff = quiz_1[1]
         quizAnswer1Button.textOn = quiz_1[1]
+        quizAnswer1Button.isChecked = false
         quizAnswer2Button.textOff = quiz_1[2]
         quizAnswer2Button.textOn = quiz_1[2]
+        quizAnswer2Button.isChecked = false
         quizAnswer3Button.textOff = quiz_1[3]
         quizAnswer3Button.textOn = quiz_1[3]
+        quizAnswer3Button.isChecked = false
         quizAnswer4Button.textOff = quiz_1[4]
         quizAnswer4Button.textOn = quiz_1[4]
+        quizAnswer4Button.isChecked = false
 
         quizSubmitButton.text =  getString(R.string.quiz_button_Submit)
 
