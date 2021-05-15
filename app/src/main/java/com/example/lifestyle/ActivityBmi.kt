5adfd40399c39740eb77c6c1e185aaba1a56b2b0
Tomasz Mnich <s16315@pjwstk.edu.pl>
2021-05-15 16:38:25 +0200

    BMI ok

--- app/src/main/java/com/example/lifestyle/ActivityBmi.kt
+++ app/src/main/java/com/example/lifestyle/ActivityBmi.kt
@@ -1,11 +1,48 @@
 package com.example.lifestyle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import java.lang.Double.valueOf
+import java.lang.Math.round
 
 class ActivityBmi : AppCompatActivity() {
+    val minHeight:Double = 0.4
+    val maxHeight:Double = 2.5
+    val minWeight:Double = 4.9
+    val maxWeight:Double = 400.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bmi)
+
+    }
+/**
+ * Calculate BMI based on an algorithm:
+ *  BMI = height / weight^2
+ **/
+
+    fun calculateBmi(view: View) {
+        val intent = Intent(this, ActivityBmi::class.java)
+        val viewBmiScore = findViewById<TextView>(R.id.textViewBmiScore) as TextView
+        val viewBmiCategory = findViewById<TextView>(R.id.textViewBmiCategory) as TextView
+        val weightEditText = findViewById<TextView>(R.id.editTextNumberWeight) as EditText
+        val weightString = weightEditText.text.toString()
+        val heightEditText = findViewById<TextView>(R.id.editTextNumberHeight) as EditText
+        val heightString = heightEditText.text.toString()
+        if(heightString.isNotBlank() && weightString.isNotBlank()){
+            val weight = weightString.toDouble()
+            val height = heightString.toDouble()
+            val bmi: Double = round((weight / ((height/100.0) * (height/100.0)))*100.0).toDouble()/100
+            viewBmiScore.text = bmi.toString()
+            if (bmi <= 18.49) viewBmiCategory.text = getString(R.string.bmi_1_category_name)
+            else if (bmi <= 24.49) viewBmiCategory.text = getString(R.string.bmi_2_category_name)
+            else if (bmi <= 29.49) viewBmiCategory.text = getString(R.string.bmi_3_category_name)
+            else viewBmiCategory.text = getString(R.string.bmi_4_category_name)
+        }
     }
 }
--- app/src/main/java/com/example/lifestyle/ActivityHb.kt
+++ app/src/main/java/com/example/lifestyle/ActivityHb.kt
@@ -1,11 +1,17 @@
 package com.example.lifestyle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class ActivityHb : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hb)
     }
+
+
 }
