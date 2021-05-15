f70251912ecaf845d77c0a4f515f1a7c89a76000
Tomasz Mnich <s16315@pjwstk.edu.pl>
2021-05-15 10:52:28 +0200

    main activity works

--- app/src/main/java/com/example/lifestyle/MainActivity.kt
+++ app/src/main/java/com/example/lifestyle/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.lifestyle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        findViewById<Button>(R.id.main_button_bmi_launcher).setOnClickListener{
+            val intent = Intent(this, ActivityBmi::class.java)
+            startActivity(intent)
+        }
+        findViewById<Button>(R.id.main_button_HBR_launcher).setOnClickListener{
+            val intent = Intent(this, ActivityHb::class.java)
+            startActivity(intent)
+        }
+        findViewById<Button>(R.id.main_button_live_style).setOnClickListener{
+            val intent = Intent(this, RmiActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
